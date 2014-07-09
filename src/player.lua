3ba1fe3cdb61e8423a56cc9d904fead0de125c56
Joshua Hartshorn <woodne@josh-laptop.netgear.com>
2014-07-09 01:18:56 -0600

    figuring out controls

--- src/player.lua
+++ src/player.lua
@@ -1,18 +1,24 @@
  player = {}
 
-function player.load()
+function player.load(dt)
 	player.x = 300
-	player.y = 5
+	player.y = 250
 	player.friction = 7
-	player.speed = 1000
 	player.width = 32
 	player.height = 32
 	hero = collider:addRectangle(player.x,player.y,player.height,player.width)
+	hero.img = love.graphics.newImage("res/hero1.png")
 	hero.velocity = {x = 0, y = 0}
-	hero.speed = 50
+	hero.speed = 400
+	local jumped
 end
 function player.update(dt)
 	hero:move(0,dt*gravity)
+	if hero.velocity.y > 0 then
+		hero.velocity.y = hero.velocity.y -5
+	elseif hero.velocity.y < 0 then
+		hero.velocity.y = hero.velocity.y +5
+	end
 	hero:move(hero.velocity.x * dt, hero.velocity.y * dt)
 	--[[
 	player.x = player.x + player.xvel * dt
@@ -24,11 +30,22 @@ end
 function player.move(dt)
 	if love.keyboard.isDown('right') then
 		    hero:move(hero.speed*dt, 0)
-			--hero.velocity.x  = hero.velocity.x + player.speed * dt
 	end
 	if love.keyboard.isDown('left') then 
 			hero:move(-hero.speed*dt, 0)
-			--hero.velocity.x  = hero.velocity.x  - player.speed * dt
+	end
+	if love.keyboard.isDown('down') then 
+			hero:move(0, hero.speed*dt)
+	end
+	if love.keyboard.isDown(' ') and
+		jumped == false then
+			hero.velocity.y = hero.velocity.y -500
+			if (hero.velocity.y < -500) then
+				hero.velocity.y = -500
+			end
+			print (hero.velocity.y)
+			hero:move(hero.velocity.x * dt, hero.velocity.y * dt )
+			jumped = true
 	end
 end
 function player.draw()
@@ -38,10 +55,7 @@ function player.draw()
 end
 
 function on_collide(dt, shape_a, shape_b, mtv_x, mtv_y)
-
     collideHeroWithTile(dt, shape_a, shape_b, mtv_x, mtv_y)
-    --print ("Collidingggg!!!!!")
-
 end
 
 function collideHeroWithTile(dt, shape_a, shape_b, mtv_x, mtv_y)
@@ -58,6 +72,9 @@ function collideHeroWithTile(dt, shape_a, shape_b, mtv_x, mtv_y)
     end
 
     hero_shape:move(mtv_x, 0)
+    if hero.velocity.y == 0 then
+    	jumped = false
+    end
     hero_shape:move(0, mtv_y)
 
 end
